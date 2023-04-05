8c5bf2860cbd0a099f6fa8d76f5e0971efb6b3ba
Crescent12138 <2986325137@qq.com>
2023-04-06 02:28:42 +0800

    runnable

--- src/runnable.hpp
+++ src/runnable.hpp
@@ -27,10 +27,70 @@ namespace Mao{
 
         void run (){
             auto thr_func = [](void *arg) -> void *{
-                
+                Runnable *ps = static_cast<Runnable*>(arg);
+                if(pthread_setcancelstate(PTHREAD_CANCEL_ENABLE,nullptr) != 0) {
+                    std::logic_error("pthread_setcancelstate error");
+                }
+                if (pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED,nullptr) != 0){
+                    std::logic_error("pthread_setcanceltype error");
+                }
+                ps->run_impl();
+                ps->pro.set_value(true);
+                return nullptr;
             };
+            int ret =0;
+            if(ret != pthread_create(&pth,nullptr,thr_func,this)){
+                throw_system_error("pthread create error",ret);
+            }
+            
+        }
+        //join thread
+        void join(){
+            int ret = pthread_join(pth,nullptr);
+            if(ret != 0){
+                throw_system_error("pthread_join error",ret);
+            }
         }
 
+        //detach thread
+        void detach(){
+            int ret = pthread_detach(pth);
+            if(ret != 0){
+                throw_system_error("pthread detach error",ret);
+            }
+        }
+
+        //cancel thread and return ok
+        virtual bool cancel() noexcept {
+            return 0==pthread_cancel(pth);
+        }
+
+        /// @brief wait and make share whether ok or not
+        /// @tparam rep type of tik
+        /// @tparam period turn of tik
+        /// @param span 
+        /// @return 
+        template<class rep,class period>
+        bool wait_for(const std::chrono::duration<rep,period>&span){
+            std::future_status stat = fut.wait_for(span);
+            bool ret = false;
+            switch(stat){
+                case std::future_status::ready:
+                    ret = true;
+                    break;
+                case std::future_status::timeout:
+                    break;
+                default:
+                    throw_system_error("future::wait_for error");
+            }
+            return ret;
+        }
+
+        /// @brief send signal to thread
+        /// @param sig 
+        void send_signal(int sig)noexcept{
+            pthread_kill(pth,sig);
+        }
     };
 }
 #endif
---
+++ src/thread_pool.hpp
@@ -0,0 +1,12 @@
+#ifndef MaoThread_Pool
+#define MaoThread_Pool
+
+namespace Mao{
+    template<class runnable,class ...Args>
+    class thread_pool{
+        
+    };
+}
+
+
+#endif
