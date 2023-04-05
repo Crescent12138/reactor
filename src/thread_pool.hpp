57b165ed95ea6e5d239f3169f6e6069e83ba9a18
Crescent12138 <2986325137@qq.com>
2023-04-06 04:26:10 +0800

    thread_pool update

--- src/thread_pool.hpp
+++ src/thread_pool.hpp
@@ -1,11 +1,40 @@
 #ifndef MaoThread_Pool
 #define MaoThread_Pool
-
+#include<vector>
+#include<memory>
+#include<queue>
+#include<mutex>
+#include<condition_variable>
+#include<type_traits>
+#include<functional>
+#include"utils.hpp"
+#include"runnable.hpp"
 namespace Mao{
-    template<class runnable,class ...Args>
-    class thread_pool{
+
+    
         
+    template<class run,class ...Args>
+    class thread_pool{
+        static_assert(std::is_base_of<Runnable,run>::value,"Not runnable");
+        static_assert(std::is_constructible<run,Args&&...>::value,"Not constructible");
+        protected:
+        std::vector<std::unique_ptr<run>>thrs;
+        public:
+        explict thread_pool(int thr_num,Args&&... args){
+            for(int i = 0 ; i < thr_num ; ++ i){
+                thrs.emplace_back(
+                    std::make_unique<run>(std::forward<Args>(args)...)
+                    );
+            }
+        }
+       thread_pool(const thread_pool&) = delete;
+       thread_pool& operator=(const thread_pool &) = delete;
+       thread_pool(thread_pool&&other) = default;
+       thread_pool& operator=(thread_pool &&other) = default;
+       virtual ~thread_pool() = default;
+
     };
+
 }
 
 
